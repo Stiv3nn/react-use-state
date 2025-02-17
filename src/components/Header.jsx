23a8f25fadd0efd55d5cf8e130319aebb87cbf22
Stiven hoxha <stivenhoxha007@gmail.com>
2025-02-17 17:00:34 +0100

    creazione file e schema base

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+    <header>
+        <div>
+            <h1>Learn Web development</h1>
+        </div>
+    </header>
+)}
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+export default function Main() {
+    return (
+    <main>
+        <div className="container">
+            <div className="buttons">
+                <button>HTML</button>
+                <button>CSS</button>
+                <button>JavaScript</button>
+                <button>Node.js</button>
+                <button>Express</button>
+                <button>ReactUS</button>
+            </div>
+            <div>
+                <div className="contenuto">
+                    <h2>HTML</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tenetur quo, esse possimus illum nesciunt modi soluta facilis vel consectetur asperiores in qui amet minima veritatis architecto deserunt quia voluptates!</p>
+                </div>
+            </div>
+        </div>
+    </main>
+)}
